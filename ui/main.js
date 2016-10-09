3e569176c03638b264b8df71e71822b497cf4ba6
Anuradha7 <pg.anuradha93@gmail.com>
2016-10-10 00:49:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,13 @@ console.log('Loaded!');
 //changing content of body
 var element = document.getElementById('main-text');
 element.innerHTML ='hola!';
+
+var img = document.getElementById('madi');
+marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';  
+}
+img.onclick = function () {
+    var interval = setInterval(moveRight,100);
+};
